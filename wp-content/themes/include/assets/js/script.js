56650f60454c8620d32a711461da30043fae7057
shiva-github <shiva-github@gmail.com>
2019-10-17 15:19:06 +0530

    extra changes - 17102019

--- wp-content/themes/include/assets/js/script.js
+++ wp-content/themes/include/assets/js/script.js
@@ -12,15 +12,16 @@ jQuery(document).ready( function($) {
 	})
 	$('#module-current').ready(function() {
 
-			$('#module-current a').each(function(i, data){
-				if( $(data).attr("href") == window.location.href ) {
-					$(data).parent().addClass('current-menu');
-					$(data).parent().addClass('active').parent().addClass('active').parent().addClass('active');
-				}
-			});
+		$('#module-current a').each(function(i, data){
+			if( $(data).attr("href") == window.location.href ) {
+				$(data).parent().addClass('current-menu');
+				$(data).parent().addClass('active').parent().addClass('active').parent().addClass('active');
+			}
+		});
 	});
 
 	jQuery.noConflict();
 	jQuery('[data-toggle="popover"]').popover();
+	
 
 }); 
